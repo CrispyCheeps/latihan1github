190d35de1b2862bcd2745b18e56724c83ca0f2d4
aldorusno <aldorusno@gmail.com>
2022-02-21 16:02:53 +0700

    Menambahkan file manager.java

---
+++ src/Manager.java
@@ -0,0 +1,10 @@
+public class Pegawai {
+    
+    public static void main (String[] args){
+        System.out.println("Daftar Manager");
+        System.out.println("Aldo - 202220001");
+        System.out.println("Ishak - 202220002");
+        System.out.println("Harbi - 202220003");
+    }
+
+}
